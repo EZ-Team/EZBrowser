509cce18d8e41607440b1a5cdf59ef236d49a7eb
Jérémy Boehm <jeremyboehm77@gmail.com>
2017-07-17 17:01:33 +0200

    Add Repository request

--- EZBrowser-Backapp/src/main/java/com/ezteam/services/reading/ArticleReadingRepository.java
+++ EZBrowser-Backapp/src/main/java/com/ezteam/services/reading/ArticleReadingRepository.java
@@ -1,7 +1,13 @@
 package com.ezteam.services.reading;
 
+import com.ezteam.entities.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ArticleReadingRepository {
+public interface ArticleReadingRepository extends JpaRepository<Article, Long>{
+
+    Optional<Article> findByKeyword(String keyword);
 }
